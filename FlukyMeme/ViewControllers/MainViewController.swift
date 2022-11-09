80899ef0147be1d4c03065a8ffb3c1c9b6bbf6ed
Artemy Volkov <wasartemwolf@gmail.com>
2022-11-09 21:58:11 +0300

    Fluky Meme App in MVC Pattern

--- FlukyMeme/ViewControllers/MainViewController.swift
+++ FlukyMeme/ViewControllers/MainViewController.swift
@@ -21,8 +21,7 @@ class MainViewController: UIViewController {
         activityIndicator.hidesWhenStopped = true
         fetchFlukyMeme()
     }
-
-
+    
     @IBAction func flukeMemeButtonTapped(_ sender: UIButton) {
         activityIndicator.startAnimating()
         fetchFlukyMeme()
@@ -31,49 +30,26 @@ class MainViewController: UIViewController {
 
 extension MainViewController {
     private func fetchFlukyMeme() {
-        guard let url = URL(string: "https://meme-api.herokuapp.com/gimme") else { return }
-        
-        URLSession.shared.dataTask(with: url) { [weak self] data, _, error in
-            guard let data = data else {
-                print(error?.localizedDescription ?? "No error description")
-                return
+        NetworkManager.shared.fetch(Meme.self, from: Link.url.rawValue) { [weak self] result in
+            switch result {
+            case .success(let meme):
+                self?.memeLabel.text = meme.title
+                self?.fetchMemeImage(from: meme.url)
+            case .failure(let error):
+                print(error)
             }
-
-            let decoder = JSONDecoder()
-            
-            do {
-                let meme = try decoder.decode(Meme.self, from: data)
-                print(meme)
-                self?.fetchMemeImage(url: meme.url)
-                
-                DispatchQueue.main.async {
-                    self?.memeLabel.text = meme.title
-                }
-                
-            } catch let error {
-                print(error.localizedDescription)
-            }
-            
-        }.resume()
+        }
     }
     
-    private func fetchMemeImage(url: String) {
-        guard let url = URL(string: url) else { return }
-        
-        URLSession.shared.dataTask(with: url) { [weak self] data, _, error in
-            guard let data = data else {
-                print(error?.localizedDescription ?? "No error description")
-                return
-            }
-            
-            guard let image = UIImage(data: data) else { return }
-            
-            DispatchQueue.main.async {
-                self?.memeImageView.image = image
+    private func fetchMemeImage(from url: String) {
+        NetworkManager.shared.fetchImage(from: url) { [weak self] result in
+            switch result {
+            case .success(let imageData):
+                self?.memeImageView.image = UIImage(data: imageData)
                 self?.activityIndicator.stopAnimating()
-                
+            case .failure(let error):
+                print(error)
             }
-            
-        }.resume()
+        }
     }
 }
